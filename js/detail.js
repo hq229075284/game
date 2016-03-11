e1e563b2dba53b1be95d867f4742ba4fcb9d3c35
hq229075284 <229075284@qq.com>
2016-03-11 18:12:33 +0800

    detail 2016.3.11

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,15 @@
-var app=angular.module("detail",[]);
-app.controller('detail_head', /*['',*/ function($scope){
-	$scope.data=data;
-}/*]*/);
+// angular
+(function() {
+	// $.noConflict();
+    var app = angular.module("detail", []);
+    app.controller('detail_head', function($scope,$rootScope) {
+        $rootScope.data = data[0];
+    });
+})();
+
+
+
+// jquery
+(function($){
+
+})(jQuery);
